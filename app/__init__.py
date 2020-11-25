24726ca8ce19598d6b29655d647e321531c69cda
Priya Samuel <psamuel@thoughtworks.com>
2020-11-25 16:27:18 +0530

    Load configuration when starting the app.
    Otherwise FlaskSQL complains about db path not found,
    and creates an in-memory db instead

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-# app.config.from_object(Config)#(can't remember if this was needed)
+app.config.from_object(Config)
+
 app.config["SECRET_KEY"] = "very-secret-word"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # Flask mega tutorial
