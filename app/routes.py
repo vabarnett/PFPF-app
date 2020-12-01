01e4e513ad2fa15607f1f1620a75178bcdacb0b0
vabarnett <58373741+vabarnett@users.noreply.github.com>
2020-12-01 13:33:51 +0000

    added code for user log in
    Added Flask login to manage user log-ins.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template, flash, redirect
 from app.form import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,8 +11,14 @@ def index():
 	return render_template('index.html', title='Home')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for user, remember_me={}'.format(form.username.data, form.remember_me.data))
-		return redirect('/index')
-	return render_template('login.html', title='Login', form=form)
+		user= User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid useername or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Sign In', form=form)
