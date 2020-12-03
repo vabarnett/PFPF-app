1943ffdb140d71f67b65d58df0fc421a42a05cf9
vabarnett <58373741+vabarnett@users.noreply.github.com>
2020-12-03 14:39:39 +0000

    updated code for measurements
    also fixed bug - indent issues

--- app/models.py
+++ app/models.py
@@ -5,20 +5,20 @@ from app import login
 
 
 class User(UserMixin, db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(120), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
-	Measurements = db.relationship('Measurements', backref = 'user', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    Measurements = db.relationship('Measurements', backref = 'user', lazy='dynamic')
 
-	def set_password(self, password):
+    def set_password(self, password):
    	    self.password_hash=generate_password_hash(password)
 
-   	def check_password(self, password):
-   		return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+   	    return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-    	return '<User {}'.format(self.username)
+        return '<User {}'.format(self.username)
 
 class Measurements(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -27,8 +27,8 @@ class Measurements(db.Model):
 	bust = db.Column(db.Float(10))
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-   def __repr__(self):
-		return '<Measurements {}>'.format(self.waist) 
+	def __repr__(self):
+		return '<Measurements {}>'.format(self.waist, self.hips, self.bust) 
 
 @login.user_loader
 def Load_User(id):
