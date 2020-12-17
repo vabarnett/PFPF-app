ee641bb58f0bf02450c9281ebfd5736ac12847d5
vabarnett <58373741+vabarnett@users.noreply.github.com>
2020-12-17 17:06:08 +0000

    added measurements to user class

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -9,6 +10,11 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    bust = db.Column(db.Float(10))
+    waist = db.Column(db.Float(10))
+    hips = db.Column(db.Float(10))
+    w2k = db.Column(db.Float(10))
+    k2a = db.Column(db.Float(10))
     Measurements = db.relationship('Measurements', backref = 'user', lazy='dynamic')
 
     def set_password(self, password):
@@ -19,6 +25,11 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}'.format(self.username)
+
+    def avatar(self, size):
+    	digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    	return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+    		digest, size)
 
 class Measurements(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
