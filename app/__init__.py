038e876dc3fc59e1a41f8066ae7c15b05e5c1551
vabarnett <58373741+vabarnett@users.noreply.github.com>
2020-12-24 09:09:25 +0000

    added email error reporting
    not yet working

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +14,23 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)  
 login = LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:#this section should add error reporting by email but does not seem to be working yet
+	if app.config['MAIL_SERVER']:
+		auth = None
+		if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+			auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+		secure = None
+		if app.config['MAIL_USE_TLS']:
+			secure = ()
+		mail_handler = SMTPHandler(
+			mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+			fromaddr='no-reply@'+ app.config['MAIL_SERVER'],
+			toaddrs=app.config['ADMINS'], subject='PFPF Error',
+			credentials=auth, secure=secure)
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
+
 
 from app import routes, models, errors
 
