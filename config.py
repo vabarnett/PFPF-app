1d2d11046d537d68bd198cd57104de83b18b657a
vabarnett <58373741+vabarnett@users.noreply.github.com>
2021-03-23 11:44:55 +0000

    Revert "added email error reporting"
    This reverts commit 1b598dd2a552d4c454183aaf3a303bc1d544d01a.

--- config.py
+++ config.py
@@ -4,17 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-	SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-	DATABASE = "Mytest"
-	SQLALCHEMY_DATABASE_URI = os.environ.get(
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    DATABASE = "Mytest"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-        ) or "sqlite:///" + os.path.join(basedir, "app.db")
-	
-	SQLALCHEMY_TRACK_MODIFICATIONS = False#the following code should add an error report by email but this does not appear to be woring yet
-	print("test", SQLALCHEMY_DATABASE_URI)
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['notifications.pfpf@aol.com']
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    print("test", SQLALCHEMY_DATABASE_URI)
