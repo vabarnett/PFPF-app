882fd7433bd1e688e27e3537683f9c3c61a82dae
vabarnett <58373741+vabarnett@users.noreply.github.com>
2021-04-20 10:51:05 +0100

    removed irrelevant code

--- app/__init__.py
+++ app/__init__.py
@@ -13,25 +13,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-<<<<<<< HEAD
-=======
-"""if not app.debug:#this section should add error reporting by email but does not seem to be working yet
-	if app.config['MAIL_SERVER']:
-		auth = None
-		if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-			auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-		secure = None
-		if app.config['MAIL_USE_TLS']:
-			secure = ()
-		mail_handler = SMTPHandler(
-			mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-			fromaddr='no-reply@'+ app.config['MAIL_SERVER'],
-			toaddrs=app.config['ADMINS'], subject='PFPF Error',
-			credentials=auth, secure=secure)
-		mail_handler.setLevel(logging.ERROR)
-		app.logger.addHandler(mail_handler)"""
 
-
->>>>>>> parent of 807771a (Revert "added test Javascript")
 from app import routes, models, errors
 
